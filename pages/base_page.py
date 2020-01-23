3225ca4cd9a834690f9d1cf98908c69e0c3b0788
Artyom Astrakhantsev <ArtyomAstrakhantsev163@users.noreply.github.com>
2020-01-23 22:40:00 +0300

    Добавал класс TestProduct, создал фикстуру с url для этого класса

--- pages/base_page.py
+++ pages/base_page.py
@@ -7,7 +7,7 @@ import math
 
 class BasePage():
 
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url, timeout=100):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
--- test_product.py
+++ test_product.py
@@ -3,29 +3,38 @@ sys.path.append('C:\\Users\\Marve\\OneDrive\\Документы\\Project_Test')
 from pages.main_page import MainPage
 from pages.product_page import ProductPage
 import time
+import pytest
 
 
-def test_add_product_to_basket(browser):
-    url = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage (browser, url)
-    page.open()
-    page.add_product_to_basket()
-    page.solve_quiz_and_get_code()
-    page.should_be_message_of_success()
-    page.should_be_price_equal_price_cart()
+@pytest.mark.parametrize('url', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+class TestProduct(object):
+    def test_add_product_to_basket(self, browser, url):
+        page = ProductPage (browser, url)
+        page.open()
+        page.add_product_to_basket()
+        page.solve_quiz_and_get_code()
+        page.should_be_message_of_success()
+        page.should_be_price_equal_price_cart()
 
-def test_should_be_message_of_success(browser):
-    url = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage (browser, url)
-    page.open()
-    page.add_product_to_basket()
-    page.solve_quiz_and_get_code()
-    page.should_be_message_of_success()
+    def test_should_be_message_of_success(self, browser, url):
+        page = ProductPage (browser, url)
+        page.open()
+        page.add_product_to_basket()
+        page.solve_quiz_and_get_code()
+        page.should_be_message_of_success()
 
-def test_should_be_price_equal_price_cart(browser):    
-    url = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage (browser, url)
-    page.open()
-    page.add_product_to_basket()
-    page.solve_quiz_and_get_code()
-    page.should_be_price_equal_price_cart()
+    def test_should_be_price_equal_price_cart(self, browser, url):    
+        page = ProductPage (browser, url)
+        page.open()
+        page.add_product_to_basket()
+        page.solve_quiz_and_get_code()
+        page.should_be_price_equal_price_cart()
